18b9d7776ca1ef3d438547d0f8e38030d98873f3
missbe <love1208tt@foxmail.com>
2018-08-25 13:37:34 +0800

    redis server module update

--- redis-server/src/main/java/cn/missbe/redis/task/CachedTimerTask.java
+++ redis-server/src/main/java/cn/missbe/redis/task/CachedTimerTask.java
@@ -1,12 +1,6 @@
 package cn.missbe.redis.task;
 
-import cn.missbe.redis.map.KeyValueNode;
 import cn.missbe.redis.map.RedisMapImpl;
-import cn.missbe.util.PrintUtil;
-import cn.missbe.util.SystemLog;
-
-import java.util.Iterator;
-import java.util.Map;
 
 public class CachedTimerTask implements Runnable{
     private RedisMapImpl redisMap = RedisMapImpl.RedisMapHolder.getInstance();
@@ -16,16 +10,6 @@ public class CachedTimerTask implements Runnable{
      */
     @Override
     public void run() {
-//        System.out.println("MAP:" + redisMap.getMaps());
-        Iterator<Map.Entry<String,KeyValueNode>> iterator = redisMap.getMaps().entrySet().iterator();
-        while (iterator.hasNext()){
-            Map.Entry<String,KeyValueNode> entry = iterator.next();
-            KeyValueNode valueNode = entry.getValue();
-            if(System.currentTimeMillis() - valueNode.getTimeOut() > 0){
-               iterator.remove();////移除缓存
-                PrintUtil.print(entry.getKey() + "缓存对象过期-进行主动删除.", SystemLog.Level.warning);
-            }
-//            System.out.println("当前正在处理缓存对象:" + entry.getKey() + ":" +valueNode);
-        }
+        redisMap.clearExpireKey();  ///清理过期缓存对象
     }
 }
